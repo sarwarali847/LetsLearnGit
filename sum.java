fc06f05d79b406554a00bc01b47b2a904b39e89d
sarwarali847 <sarwarali847@gmail.com>
2021-09-13 12:00:36 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum{
+ public static void main(string[] args) {
+              
+              int num1=5,num2=10,sum;
+              sum=num1+num2;
+              system.out.println("sum of these number: " +sum);
+}
+}
